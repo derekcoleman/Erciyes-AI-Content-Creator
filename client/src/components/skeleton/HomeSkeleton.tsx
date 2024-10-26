728f0701fc5cb7ce723091362c370ed95bd0304d
Prelencos <120386410+Kadirleventkabadayi@users.noreply.github.com>
2024-10-26 10:13:25 +0300

    bug fix

--- client/src/components/skeleton/HomeSkeleton.tsx
+++ client/src/components/skeleton/HomeSkeleton.tsx
@@ -3,40 +3,38 @@ import React from "react";
 
 function HomeSkeleton() {
   return (
-    <Box>
-      <Grid container spacing={1}>
-        {/* Üstte geniş ve uzun skeleton */}
-        <Grid item xs={12}>
-          <Skeleton variant="rectangular" height={300} />
-        </Grid>
-
-        {/* Instagram tarzı yan yana kare skeletonlar */}
-        <Grid item xs={6} sm={4} md={3}>
-          <Skeleton variant="rectangular" height={200} />
-        </Grid>
-        <Grid item xs={6} sm={4} md={3}>
-          <Skeleton variant="rectangular" height={200} />
-        </Grid>
-        <Grid item xs={6} sm={4} md={3}>
-          <Skeleton variant="rectangular" height={200} />
-        </Grid>
-        <Grid item xs={6} sm={4} md={3}>
-          <Skeleton variant="rectangular" height={200} />
-        </Grid>
-        <Grid item xs={6} sm={4} md={3}>
-          <Skeleton variant="rectangular" height={200} />
-        </Grid>
-        <Grid item xs={6} sm={4} md={3}>
-          <Skeleton variant="rectangular" height={200} />
-        </Grid>
-        <Grid item xs={6} sm={4} md={3}>
-          <Skeleton variant="rectangular" height={200} />
-        </Grid>
-        <Grid item xs={6} sm={4} md={3}>
-          <Skeleton variant="rectangular" height={200} />
-        </Grid>
+    <Grid container spacing={1}>
+      {/* Üstte geniş ve uzun skeleton */}
+      <Grid item xs={12}>
+        <Skeleton variant="rectangular" height={300} />
       </Grid>
-    </Box>
+
+      {/* Instagram tarzı yan yana kare skeletonlar */}
+      <Grid item xs={6} sm={4} md={3}>
+        <Skeleton variant="rectangular" height={200} />
+      </Grid>
+      <Grid item xs={6} sm={4} md={3}>
+        <Skeleton variant="rectangular" height={200} />
+      </Grid>
+      <Grid item xs={6} sm={4} md={3}>
+        <Skeleton variant="rectangular" height={200} />
+      </Grid>
+      <Grid item xs={6} sm={4} md={3}>
+        <Skeleton variant="rectangular" height={200} />
+      </Grid>
+      <Grid item xs={6} sm={4} md={3}>
+        <Skeleton variant="rectangular" height={200} />
+      </Grid>
+      <Grid item xs={6} sm={4} md={3}>
+        <Skeleton variant="rectangular" height={200} />
+      </Grid>
+      <Grid item xs={6} sm={4} md={3}>
+        <Skeleton variant="rectangular" height={200} />
+      </Grid>
+      <Grid item xs={6} sm={4} md={3}>
+        <Skeleton variant="rectangular" height={200} />
+      </Grid>
+    </Grid>
   );
 }
 
