3f2ad463fe2f7c857a9fd9027a8a28250966d0b3
Prelencos <120386410+Kadirleventkabadayi@users.noreply.github.com>
2024-11-25 16:02:59 +0300

    ExpandedCard component completed

--- client/src/components/card/CustomCard.tsx
+++ client/src/components/card/CustomCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardContent,
@@ -13,6 +13,7 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import { textLimiter } from "@/lib/utils";
+import ExpandedCard from "./ExpandedCard";
 
 interface CustomCardProps {
   platform: string;
@@ -37,6 +38,16 @@ const CustomCard: React.FC<CustomCardProps> = ({
   date,
   height = 230,
 }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const renderPlatformIcon = () => {
     switch (platform) {
       case "instagram":
@@ -51,70 +62,88 @@ const CustomCard: React.FC<CustomCardProps> = ({
   };
 
   return (
-    <Card
-      sx={{
-        maxWidth: 900,
-        display: "flex",
-        justifyContent: "center",
-        height,
-      }}
-    >
-      <CardContent sx={{ width: "40%" }}>
-        {renderPlatformIcon()}
-        <CardMedia
-          sx={{ height: 140, objectFit: "contain" }}
-          image={postImage}
-          title="green iguana"
-        />
-      </CardContent>
-      <CardContent sx={{ width: "50%" }}>
-        <Typography
-          gutterBottom
-          variant="h5"
-          component="div"
-          sx={{ textAlign: "center" }}
-        >
-          {title}
-        </Typography>
-        <Typography variant="body2" sx={{ color: "text.secondary" }}>
-          {textLimiter(content)}
-        </Typography>
-        <Typography
-          variant="body2"
-          color="text.secondary"
-          sx={{ textAlign: "center" }}
-        >
-          {hashtags.map((tag, index) => (
-            <span key={index} style={{ marginRight: "5px", color: "#007bff" }}>
-              #{tag}
-            </span>
-          ))}
-        </Typography>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <IconButton size="small">
-            <FavoriteIcon fontSize="small" />
-          </IconButton>
-          <Typography variant="body2">{likes}</Typography>
-          <IconButton size="small" sx={{ marginLeft: 1 }}>
-            <CommentIcon fontSize="small" />
-          </IconButton>
-          <Typography variant="body2">{comments}</Typography>
-        </Box>
-        <Typography
-          fontSize="small"
-          color="text.secondary"
-          sx={{ textAlign: "center" }}
-        >
-          {new Date(date).toLocaleString()}
-        </Typography>
-      </CardContent>
-    </Card>
+    <>
+      <Card
+        sx={{
+          maxWidth: 900,
+          display: "flex",
+          justifyContent: "center",
+          height,
+        }}
+        onClick={handleClick}
+      >
+        <CardContent sx={{ width: "40%" }}>
+          {renderPlatformIcon()}
+          <CardMedia
+            sx={{ height: 140, objectFit: "contain" }}
+            image={postImage}
+            title="green iguana"
+          />
+        </CardContent>
+        <CardContent sx={{ width: "50%" }}>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            sx={{ textAlign: "center" }}
+          >
+            {title}
+          </Typography>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            {textLimiter(content)}
+          </Typography>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={{ textAlign: "center" }}
+          >
+            {hashtags.map((tag, index) => (
+              <span
+                key={index}
+                style={{ marginRight: "5px", color: "#007bff" }}
+              >
+                #{tag}
+              </span>
+            ))}
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <IconButton size="small">
+              <FavoriteIcon fontSize="small" />
+            </IconButton>
+            <Typography variant="body2">{likes}</Typography>
+            <IconButton size="small" sx={{ marginLeft: 1 }}>
+              <CommentIcon fontSize="small" />
+            </IconButton>
+            <Typography variant="body2">{comments}</Typography>
+          </Box>
+          <Typography
+            fontSize="small"
+            color="text.secondary"
+            sx={{ textAlign: "center" }}
+          >
+            {new Date(date).toLocaleString()}
+          </Typography>
+        </CardContent>
+      </Card>
+      <ExpandedCard
+        open={open}
+        onClose={handleClose}
+        title={title}
+        postImage={postImage}
+        content={content}
+        hashtags={hashtags}
+        likes={likes}
+        comments={comments}
+        date={date}
+        platformIcon={renderPlatformIcon()}
+      />
+    </>
   );
 };
 
