8ecb8356b1d1d702ddc6937abb36e3dc390aa535
Prelencos <120386410+Kadirleventkabadayi@users.noreply.github.com>
2024-12-18 13:49:34 +0300

    Theme switch completed

--- client/src/components/forms/NewSettingsForm.tsx
+++ client/src/components/forms/NewSettingsForm.tsx
@@ -1,60 +1,34 @@
-import { useState } from "react";
-import {
-  Box,
-  Button,
-  Card,
-  FormControl,
-  MenuItem,
-  Select,
-  Typography,
-} from "@mui/material";
+"MBY sağolsun 🤲";
+import { Box, Card, FormControlLabel, Typography } from "@mui/material";
 import { addSettings } from "@/lib/utils";
 import { SettingsInfo } from "@/lib/types";
-import { useAtom } from "jotai";
-import { themeAtom } from "@/store";
 
-const themes = ["Light", "Dark"];
-const notificationOptions = ["Enabled", "Disabled"];
+interface NewSettingsFormProps {}
 
-interface NewSettingsFormProps {
-  notificationsData: string;
-}
-
-const NewSettingsForm: React.FC<NewSettingsFormProps> = ({
-  notificationsData = "Enabled",
-}) => {
-  const [theme, setTheme] = useAtom(themeAtom);
-  const [newTheme, setNewTheme] = useState(theme);
-  const [notifications, setNotifications] = useState(notificationsData);
-
+const NewSettingsForm: React.FC<NewSettingsFormProps> = () => {
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
-    if (isFormValid()) {
-      try {
-        const settingsInfo: SettingsInfo = await addSettings({
-          theme,
-          notifications,
-        });
-        if (settingsInfo.status) {
-          console.log(settingsInfo);
-        } else {
-          alert("Settings update failed: " + settingsInfo.message);
-        }
-      } catch (error) {
-        alert("Settings update failed: " + error.message);
-      }
-    }
+    // if (isFormValid()) {
+    //   try {
+    //     const settingsInfo: SettingsInfo = await addSettings({
+    //       //theme giib gelen imputlar gelecek
+    //     });
+    //     if (settingsInfo.status) {
+    //       console.log(settingsInfo);
+    //     } else {
+    //       alert("Settings update failed: " + settingsInfo.message);
+    //     }
+    //   } catch (error) {
+    //     alert("Settings update failed: " + error.message);
+    //   }
+    // }
   };
 
-  const isFormValid = () => {
-    return theme && notifications;
-  };
-
-  const toggleTheme = () => {
-    const updateTheme = newTheme === "Dark" ? "Dark" : "Light";
-    setTheme(updateTheme);
-  };
+  // const isFormValid = () => {
+  //   //verilen inpurlar valid mi
+  //   return true;
+  // };
 
   return (
     <Card
@@ -67,67 +41,10 @@ const NewSettingsForm: React.FC<NewSettingsFormProps> = ({
       }}
     >
       <Typography variant="h6" gutterBottom sx={{ textAlign: "center" }}>
-        Tema ve Bildirim Ayarları
+        Bilmem Ne Ayarları
       </Typography>
 
-      <Box sx={{ display: "flex", gap: 2, justifyContent: "center" }}>
-        <FormControl fullWidth margin="normal" sx={{ width: "30%" }}>
-          <Select
-            value={newTheme}
-            onChange={(e) => setNewTheme(e.target.value)}
-            displayEmpty
-            renderValue={(selected) => {
-              if (!selected) {
-                return <span style={{ color: "gray" }}>Tema Seçimi</span>;
-              }
-              return selected;
-            }}
-          >
-            <MenuItem value="" disabled>
-              Tema Seçimi
-            </MenuItem>
-            {themes.map((themeOption) => (
-              <MenuItem key={themeOption} value={themeOption}>
-                {themeOption}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-
-        <FormControl fullWidth margin="normal" sx={{ width: "30%" }}>
-          <Select
-            value={notifications}
-            onChange={(e) => setNotifications(e.target.value)}
-            displayEmpty
-            renderValue={(selected) => {
-              if (!selected) {
-                return <span style={{ color: "gray" }}>Bildirim Seçimi</span>;
-              }
-              return selected;
-            }}
-          >
-            <MenuItem value="" disabled>
-              Bildirim Seçimi
-            </MenuItem>
-            {notificationOptions.map((notificationOption) => (
-              <MenuItem key={notificationOption} value={notificationOption}>
-                {notificationOption}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          sx={{ width: "10%", height: "56px", marginTop: "14px" }}
-          disabled={!isFormValid()}
-          onClick={toggleTheme}
-        >
-          Kaydet
-        </Button>
-      </Box>
+      <Box sx={{ display: "flex", gap: 2, justifyContent: "center" }}></Box>
     </Card>
   );
 };
